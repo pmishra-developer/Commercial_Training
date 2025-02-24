667a125b8291d3757a4c4be7e76c7caaf85a8622
SheronAdrien <sheron.adrienarokias@sainsburys.co.uk>
2025-02-24 19:34:05 +0530

    Entity Framework with SQLite

---
+++ 1_Basics/EntityFrameWork/SQLLite/SQLLite/Product.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLLite
+{
+    public class Product : DbContext
+    {
+        public DbSet<Customer> Customers {  get; set; }
+
+        public string Databasepath {  get; }
+
+        public Product() 
+        {
+            var folder = Environment.SpecialFolder.Desktop;
+            var path = Environment.GetFolderPath(folder);
+
+            Databasepath = Path.Combine(path, "products.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"DataSource={Databasepath}");
+        }
+    }
+}
---
+++ 1_Basics/EntityFrameWork/SQLLite/SQLLite/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace SQLLite;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+
+        using var database = new Product();
+        database.Database.EnsureCreated();
+
+        Console.WriteLine("Inserting new record");
+        Customer customer = new Customer();
+
+        customer.Id = Guid.NewGuid();
+        customer.Name = "Jerry";
+        customer.Address = "XYZ";
+        customer.City = "London";
+
+        database.Customers.Add(customer);
+        database.SaveChanges();
+
+        var customers = database.Customers;
+        foreach(var cust in customers) 
+        {
+            Console.WriteLine($"Id:{customer.Id}, Name:{customer.Name}, Address:{customer.Address},City:{customer.City}");
+        }
+
+        var remove = database.Customers.FirstOrDefault(x => x.Name.Contains("Tomm"));
+        if (remove != null )
+        {
+            database.Customers.Remove(remove);
+            database.SaveChanges();
+        }
+    }
+}
+
